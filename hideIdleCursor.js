5047b3ffaac8750548a8324f112d59d985438e18
Marco Moroni <marcomoroni394@gmail.com>
2018-07-06 13:14:15 +0200

    First commit

---
+++ hideIdleCursor.js
@@ -0,0 +1,38 @@
+//https://stackoverflow.com/questions/31798922/hide-mouse-cursor-if-idle-inside-a-div-after-some-seconds/31798987
+
+$(function() {
+    var timer;
+    var fadeInBuffer = false;
+    $(document).mousemove(function() {
+        if (!fadeInBuffer) {
+            if (timer) {
+                //console.log("clearTimer");
+                clearTimeout(timer);
+                timer = 0;
+            }
+
+            //console.log("fadeIn");
+            $('html').css({
+                cursor: ''
+            });
+        } else {
+            $('body').css({
+                cursor: 'default'
+            });
+            fadeInBuffer = false;
+        }
+
+
+        timer = setTimeout(function() {
+            //console.log("fadeout");
+            $('body').css({
+                cursor: 'none'
+            });
+
+            fadeInBuffer = true;
+        }, 5000)
+    });
+    $('.html5gallery-box-0').css({
+        cursor: 'default'
+    });
+});
